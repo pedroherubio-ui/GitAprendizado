175afa142b94391e3a23e4c09270632083d6f124
Pedro Henrique <pedroherubio@gmail.com>
2025-08-24 14:45:57 -0300

    Alteração para input

--- Test.py
+++ Test.py
@@ -1,2 +1,4 @@
-name = "Pedro"
-print(f"Eae {name} como vai?")
+name = input("Insira seu nome ")
+age = input("Insira sua idade ")
+print(f"Olá {name} como vai? você tem {age} anos de idade e está apto para o mercado de trabalho")
+
